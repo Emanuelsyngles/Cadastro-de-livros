0a0483bd1c06280e747bc941277086e2e66c66f4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-12-07 16:10:44 -0300

    ...

--- livros/index.js
+++ livros/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
-const exphbs = ('express-handlebars')
+const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
 
 const app = express()
 
 app.engine('handlebars', exphbs.create)
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/',(req, res) => {
+    res.render('home');
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql2'
+})
+
+conn.connect(function(err) {
+    if(err){
+        console.log(err)
+    }
+    console.log('conectou ao MySql')
+
+    app.listen(3000)
+})
